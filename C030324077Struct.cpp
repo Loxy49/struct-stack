9478a9ef7a37f044d2ee94d3c7a3f86a89746599
Miftahush Syaadah <miftahussyaadah12@gmail.com>
2025-05-31 16:13:11 +0800

    Menambahkan Prosedur Input Data

--- C030324077Struct.cpp
+++ C030324077Struct.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std; 
 
-#include <iostream>
-#include <string>
-using namespace std;
-
 struct mahasiswa{
     string nama;
     string nim;
@@ -12,9 +8,25 @@ struct mahasiswa{
     float ipk;
 };
 
+void inputData(mahasiswa *m) {
+   system("cls");
+   cout << "== Input Data Mahasiswa ==\n" << endl;
+   cout << "Masukkan nama:" << endl;
+   getline (cin, m ->nama);
+   cout << "Masukkan NIM:" << endl;
+   getline (cin, m->nim);
+   cout << "Masukkan alamat:" << endl;
+   getline (cin, m->alamat);
+   cout << "Masukkan IPK:" << endl;
+   cin >> m->ipk;
+   cin.ignore();    
+}
+
 int main(){
-    mahasiswa syaadah;
-   
-};
-
-
+    mahasiswa Ti2b;
+    string *nama = &Ti2b.nama;
+    string *nim  = &Ti2b.nim;
+    string *alamat = &Ti2b.alamat;
+    float *ipk = &Ti2b.ipk; 
+    
+}
