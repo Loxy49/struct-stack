1e62dd386b4f39eb4c614fb8bcd9f9f3c33fce13
Miftahush Syaadah <miftahussyaadah12@gmail.com>
2025-05-31 15:27:38 +0800

    Struktur Data

---
+++ C030324077Struct.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std; 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct mahasiswa{
+    string nama;
+    string nim;
+    string alamat;
+    float ipk;
+};
+
+int main(){
+    mahasiswa syaadah;
+   
+};
+
+
